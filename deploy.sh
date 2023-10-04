5cc9c2560a9c8c32cd69d9d33edecf0b3cfe7587
Charles Lemperiere <lemperiere.charles@gmail.com>
2023-10-04 23:52:43 +0200

    script de déploiement en production

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Aller à la racine du projet
+cd /chemin/absolu/vers/votre/projet
+
+# Effectuer un git pull pour récupérer les dernières modifications
+git pull origin master
+
+# Installer/mettre à jour les dépendances via composer
+composer install --no-dev --optimize-autoloader
+
+# Supprimer le répertoire des tests
+rm -rf tests/
+
+# Vous pouvez ajouter d'autres commandes spécifiques à Symfony, comme la mise à jour de la base de données, etc.
+php bin/console doctrine:migrations:migrate --no-interaction
+
+# Si vous utilisez un système de cache, n'oubliez pas de le vider
+php bin/console cache:clear --env=prod
+
+echo "Déploiement terminé avec succès!"
